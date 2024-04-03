51a0a432852242c4df203386bea70985bd99ca46
Kang-bh <kabyho3513@gmail.com>
2024-04-03 13:22:37 +0900

    fix debug status

--- first-blog-site/mysite/settings.py
+++ first-blog-site/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-y(js&6_21)ta#!y50p^jibx_6wz=&&=v523+d6y=_rry_2am*("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
